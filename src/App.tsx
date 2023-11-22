471bc39585049c7f6b31e39499b96670477dabe4
i.kochetov <WaNaShKA@yandex.ru>
2023-11-22 22:30:22 +0300

    Made up the background of the site and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import purpleBall from "./sourse/image/background/purpleBall.svg"
+import redBallBig from "./sourse/image/background/redBallBig.svg"
+import redBallSmall from "./sourse/image/background/redBallSmall.svg"
+import logo from "./sourse/image/logoWelbex.svg"
+import telegram from "./sourse/image/link/Telegram.svg"
+import phone from "./sourse/image/link/phone.svg"
+import whatsapp from "./sourse/image/link/whatsapp.svg"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <div className={styles.svgContainer}>
+                <img src={purpleBall} className={styles.purpleBall}/>
+                <img src={redBallBig} className={styles.redBallBig}/>
+                <img src={redBallSmall} className={styles.redBallSmall}/>
+            </div>
+            <header className={styles.header}>
+                <div className={styles.blockLogo}>
+                    <img src={logo} className={styles.logo}/>
+                    <div className={styles.slogan}>крупный интегратор CRM в Росcии и ещё 8 странах</div>
+                </div>
+                <ul className={styles.list}>
+                    <li>Услуги</li>
+                    <li>Виджеты</li>
+                    <li>Интеграции</li>
+                    <li>Кейсы</li>
+                    <li>Сертификаты</li>
+                </ul>
+                <div className={styles.link}>
+                    <a href="tel:+7 555 555-55-55" className={styles.numberPhone}>+7 555 555-55-55</a>
+                    <a><img src={telegram}/> </a>
+                    <a><img src={phone}/> </a>
+                    <a><img src={whatsapp}/> </a>
+                </div>
+            </header>
+            <div className={styles.content}>
+
+            </div>
+            <footer>
+
+            </footer>
+
+        </div>
+    );
 }
 
 export default App;
