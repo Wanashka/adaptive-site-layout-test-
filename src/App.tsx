1c204eb7f8537b5a1958643f0ea3601cd3c93778
i.kochetov <WaNaShKA@yandex.ru>
2023-11-24 01:11:14 +0300

    Designed and implemented the footer

--- src/App.tsx
+++ src/App.tsx
@@ -37,12 +37,50 @@ function App() {
                 </div>
             </header>
             <div className={styles.content}>
-
+                в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в
+                <br/>в<br/>в<br/>в<br/>в<br/>в<br/>в<br/>в
             </div>
-            <footer>
+            <footer className={styles.footer}>
+                <div>
+                    <div className={styles.footerTitle}>О компании</div>
+                    <div className={styles.blockFooter}>
+                        <a href={""}>Партнёрская программа</a>
+                        <a href={""}>Вакансии</a>
+                    </div>
+                </div>
+                <div className={styles.menuBlock}>
+                    <div className={styles.footerTitle}>Меню</div>
+                    <div className={`${styles.blockFooter} ${styles.blockMenuList}`}>
+                        <a href={""}>Расчёт стоимости</a>
+                        <a href={""}>Услуги</a>
+                        <a href={""}>Виджеты</a>
+                        <a href={""}>Интеграции</a>
+                        <a href={""}>Наши клиенты</a>
+                        <a href={""}>Кейсы</a>
+                        <a href={""}>Благодарственные письма</a>
+                        <a href={""}>Сертификаты</a>
+                        <a href={""}>Блог на Youtube</a>
+                        <a href={""}>Вопрос / Ответ</a>
+                    </div>
+                </div>
+                <div className={styles.contactBlock}>
+                    <div className={styles.footerTitle}>Контакты</div>
+                    <div className={styles.blockFooter}>
+                        <a href={""}>+7 555 555-55-55</a>
+                        <div className={styles.socialNetworkList}>
+                            <a className={styles.socialNetwork}><img src={telegram}/> </a>
+                            <a className={styles.socialNetwork}><img src={phone}/> </a>
+                            <a className={styles.socialNetwork}><img src={whatsapp}/> </a>
+                        </div>
+                        <a href={""}>Москва, Путевой проезд 3с1, к 902</a>
+                    </div>
+                    <div className={styles.confidentiality}>
+                        <div>©WELBEX 2022. Все права защищены.</div>
+                        <div>Политика конфиденциальности</div>
+                    </div>
+                </div>
 
             </footer>
-
         </div>
     );
 }
